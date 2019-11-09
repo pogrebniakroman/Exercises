e46ce228cc8b45beefb10d31f689c4c1ffcf70d6
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-09 18:48:05 +0200

    new task

---
+++ src/main/java/Basic17.java
@@ -0,0 +1,10 @@
+public class Basic17 {
+    public static void  main(String[] args) {
+        float r = 10;
+        double S = Math.PI*Math.pow(r,r);
+        double P = 2*Math.PI*r;
+        System.out.println( "Area is "+ S);
+        System.out.println("Perimetr is:"+ P);
+
+    }
+}
