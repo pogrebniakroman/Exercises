9e61f97319857f23fefaf8ac74c4caf765315b57
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-09 19:41:16 +0200

    new task

---
+++ src/main/java/Basic18.java
@@ -0,0 +1,18 @@
+public class Basic18 {
+    public static void main(String[] args) {
+      float a = (float) 6.3;
+      float b = (float) 1.7;
+      float c = (float) (a*b);
+      float d = (float) Math.sqrt(c);
+      float r = (float) (d*(a/b-b/a));
+      float t = (float) (Math.pow(a,b));
+      float m = (float) (4*a*b);
+      float h = (float)(t-m);
+      float g = (float) Math.sqrt(h);
+      float k = (float) (r/g);
+
+        System.out.println(k);
+
+
+    }
+}
