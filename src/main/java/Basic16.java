38fb90b1e146ed9223e8a40458901d67fa04143c
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-09 18:29:45 +0200

    new task

---
+++ src/main/java/Basic16.java
@@ -0,0 +1,13 @@
+public class Basic16 {
+    public static void main(String[] args) {
+        int x1=0;
+        int x2=5;
+        int x3=0;
+        int y1=0;
+        int y2=6;
+        int y3=10;
+        int area=((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1))/2;
+        System.out.println("Area of triangle = "+ area);
+
+    }
+}
