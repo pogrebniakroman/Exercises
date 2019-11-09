c743818574093772b96af147759516ebb8036db3
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-09 17:57:22 +0200

    new task

---
+++ src/main/java/Basic14.java
@@ -0,0 +1,8 @@
+public class Basic14 {
+    public static void main(String[] args) {
+        double a = 30;
+        double b = Math.toRadians(a);
+        double c = 1 / Math.tan(b);
+        System.out.println("ctg(30)= "+ c);
+    }
+}
