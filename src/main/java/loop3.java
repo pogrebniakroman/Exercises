328994c162160ab7a9a43d8146017a8f0690c80d
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-15 21:41:30 +0200

    Default Changelist

--- src/main/java/loop3.java
+++ src/main/java/loop3.java
@@ -1,8 +1,8 @@
-public class loop2 {
+public class loop3 {
     public static void main(String[] args) {
-        String a = "*";
+        String a = "**";
         int i;
-        for(i=1; i<=5; i++){
+        for(i=0; i<=2; i++){
             System.out.println(a);
         }
     }
