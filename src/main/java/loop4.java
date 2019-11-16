c52c5f10538c3597b6c203e7c0bff177fc829e4b
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-16 19:54:56 +0200

    Default Changelist

--- src/main/java/loop4.java
+++ src/main/java/loop4.java
@@ -1,13 +1,10 @@
 public class loop4 {
     public static void main(String[] args) {
-       else{ char a = '*';
-       if(a = 1)
-            (int j = 2)
-                {
-                    System.out.println(a);
-                }
+        for(int i = 0;  i < 2; i++){
+            for(int j = 0; j < 2; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
-
-
-
+}
---
+++ src/main/java/loop4a.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class loop4a {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a =in.nextInt();
+        System.out.println("Enter b:");
+        int b = in.nextInt();
+        System.out.println("Result:");
+
+        if(a > b){
+            System.out.println(" a is more than b");
+            }
+            else {
+            System.out.println(" a is less than b");
+        }
+        }
+
+    }
+
