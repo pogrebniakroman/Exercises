9a042d3ceda6a1f46fb091149655c6388f61bb38
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-16 18:16:56 +0200

    Default Changelist

---
+++ src/main/java/loop4.java
@@ -0,0 +1,13 @@
+public class loop4 {
+    public static void main(String[] args) {
+       else{ char a = '*';
+       if(a = 1)
+            (int j = 2)
+                {
+                    System.out.println(a);
+                }
+        }
+    }
+
+
+
