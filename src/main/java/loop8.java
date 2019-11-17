690efbfb3b7cb6adb4d8948840709c09283cd81c
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-17 18:01:49 +0200

    Default Changelist

---
+++ src/main/java/loop8.java
@@ -0,0 +1,10 @@
+import static java.lang.Math.*;
+
+public class loop8 {
+    public static void main(String[] args) {
+        for (byte i = 1; i <= 10; i++) {
+            System.out.print(Math.pow(i, 2));
+        }
+    }
+}
+
