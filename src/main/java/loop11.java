b70e8953e445a45bf61ba20f219de201808675f2
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-20 20:39:46 +0200

    Default Changelist

--- src/main/java/loop11.java
+++ src/main/java/loop11.java
@@ -1,11 +1,11 @@
 
 
-public class loop6a {
+public class loop11 {
 
     public static void main(String[] args) {
 
             for (int j = 0; j <= 10; j++) {
-                System.out.println( 5 + "*" + j);
+                System.out.println( 5 + "*" + j + "=" + 5*j + ";");
             }
         }
     }
