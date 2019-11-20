3c925227cce889648c12e5075439b5b951648230
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-20 21:28:50 +0200

    Default Changelist

---
+++ src/main/java/loop10.java
@@ -0,0 +1,15 @@
+
+
+
+public class loop10 {
+
+    public static void main(String[] args) {
+
+        for (int j = 1; j <= 10; j++) {
+            for (int i = 0; i <= 4; i++) {
+                System.out.println(j);
+            }
+        }
+    }
+}
+
---
+++ src/main/java/loop17c.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class loop17c {
+    public static void main(String[] args) {
+        int size = 4;
+        for (int j = 0; j < (size - 1); j++)
+            System.out.print(" ");
+        System.out.println("*");
+        for (int j = 1; j < (size - 1); j++)
+        {
+            for (int k = 0; k < (size - 1 - j); k++)
+                System.out.print(" ");
+            System.out.print("*");
+            for (int m = 0; m < 2*j-1; m++){
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+        for (int j = 0; j < size ; j++)
+            System.out.print("* ");
+        System.out.println(" ");
+    }
+}
+
+
+
+
