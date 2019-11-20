7edaedb3485d264ec3d01c55bdeff400a5db1afe
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-20 20:36:16 +0200

    Default Changelist

---
+++ src/main/java/loop6a.java
@@ -0,0 +1,12 @@
+
+
+public class loop6a {
+
+    public static void main(String[] args) {
+
+            for (int j = 0; j <= 10; j++) {
+                System.out.println( 5 + "*" + j);
+            }
+        }
+    }
+
