35e1d0ca134eba9727aef268c0353be3289380a3
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-27 19:44:48 +0200

    Default Changelist

---
+++ src/main/java/Strings2.java
@@ -0,0 +1,10 @@
+public class Strings2 {
+    public static void main(String[] args) {
+        String char1 = "A friendly word is better than a heavy cake";
+        char[] chars = char1.toCharArray();
+        Integer count1 = chars.length;
+        System.out.println(count1);
+
+
+    }
+}
