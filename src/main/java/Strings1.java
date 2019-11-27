3a33fc4cc331096949f046853328c4d44701d2df
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-27 19:20:35 +0200

    Default Changelist

---
+++ src/main/java/Strings1.java
@@ -0,0 +1,8 @@
+public class Strings1 {
+    public static void main(String[] args) {
+        String string1 = "A crow will";
+        String string2 = " never ";
+        String string3 = "be a falcon";
+        System.out.println(string1+string2+string3);
+    }
+}
---
+++ src/main/java/TaskCar.java
@@ -0,0 +1,16 @@
+public class TaskCar {
+    public static void main(String[] args) {
+
+    }
+       public  void  start(){
+
+    }
+       public  void stop(){
+
+       }
+       public int drive(int howlong){
+        int distance= howlong*60;
+        int a=distance;
+        return a;
+       }
+}
