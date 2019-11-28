8580b2551a6f92ea7cf85eed0e51329d6ecdec67
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-28 20:24:53 +0200

    Default Changelist

---
+++ src/main/java/Strings5.java
@@ -0,0 +1,7 @@
+public class Strings5 {
+    public static void main(String[] args) {
+        String a = "Deficiencies come by the kilo and go by the gram";
+        String b = a.toUpperCase();
+        System.out.println(b);
+    }
+}
