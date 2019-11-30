bb1d3e9ef0169208a0f3f7b1557cc8497494a234
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-11-30 12:07:47 +0200

    Default Changelist

---
+++ src/main/java/Strings4.java
@@ -0,0 +1,11 @@
+public class Strings4 {
+    public static void main(String[] args) {
+        String a = "Borrowed bread lies heavy on the stomach";
+        char[] ar = a.toCharArray();
+      for(char i:ar) {
+            System.out.print(i+",");
+
+
+        }
+    }
+}
