dc319301025f3339fbade37263b4bbc117a682ce
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-12-01 08:48:43 +0200

    Default Changelist

--- src/main/java/Strings9.java
+++ src/main/java/Strings9.java
@@ -1,6 +1,6 @@
 public class Strings9 {
     public static void main(String[] args) {
-       StringBuffer stringBuffer = new StringBuffer( "F lies will not land on a boiling pot");
+       StringBuffer stringBuffer = new StringBuffer( "Flies will not land on a boiling pot");
        stringBuffer.reverse();
         System.out.println(stringBuffer);
 
