02c9ad63e7e01c9f2b617e2cd121a3312f460ed2
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-12-01 08:47:55 +0200

    Default Changelist

---
+++ src/main/java/Strings9.java
@@ -0,0 +1,8 @@
+public class Strings9 {
+    public static void main(String[] args) {
+       StringBuffer stringBuffer = new StringBuffer( "F lies will not land on a boiling pot");
+       stringBuffer.reverse();
+        System.out.println(stringBuffer);
+
+    }
+}
