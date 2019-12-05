f5edb4b221e4cdc40d7cfa25c9d41f74388c6c65
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-12-05 23:01:01 +0200

    Default Changelist

---
+++ src/main/java/Begin33.java
@@ -0,0 +1,14 @@
+public class Begin33 {
+    public static void main(String[] args) {
+        int x = 1;
+        int A =90;
+        int price1 = A/x;
+
+            System.out.println(price1);
+
+         int Y = 5;
+         int price2 = Y*price1;
+        System.out.println(price2);
+
+        }
+    }
---
+++ src/main/java/Strings12.java
@@ -0,0 +1,11 @@
+
+public class Strings12 {
+    public static void main(String[] args) {
+        String string = "He who licks knives will soon cut his tongue";
+        char[] data= string.toCharArray();
+        for(char current:data )
+            System.out.println(data);
+        }
+    }
+
+
