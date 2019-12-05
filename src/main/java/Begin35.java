a6a789b316c9a6ae4a0b693a607489642698fff9
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-12-05 23:13:31 +0200

    Default Changelist

---
+++ src/main/java/Begin35.java
@@ -0,0 +1,14 @@
+
+
+public class Begin35 {
+    public static void main(String[] args) {
+        int V = 25;
+        int U = 15;
+        int T1 = 10;
+        int T2 = 10;
+        int S1 = T1*V;
+        int S2 = T1*V-T2*U;
+        System.out.println("Путь по озеру " + S1);
+        System.out.println("Путь по реке " + S2);
+    }
+}
