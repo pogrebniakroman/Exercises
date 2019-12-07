b39dee0a57385044f6be1d06b1d2f4f2287df168
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-12-07 20:29:43 +0200

    Default Changelist

--- src/main/java/loop6.java
+++ src/main/java/loop6.java
@@ -1,10 +1,13 @@
 public class loop6 {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++)
+
+        for (int i = 1; i < 10; i++) {
             System.out.print(i);
+
+        }
+        System.out.println("");
         for (int j = 10; j <= 10; j--) {
             if (j <10)
-
                 System.out.print(j);
             if (j<=1)
                 break;
