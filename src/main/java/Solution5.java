b9d3d8ec1adab8bca26b1d9d8bae9d84a83422d9
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-12-20 12:48:50 +0200

    Default Changelist

---
+++ src/main/java/Solution5.java
@@ -0,0 +1,17 @@
+
+    import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+    public class Solution5 {
+        public static void main(String[] args) throws Exception {
+            BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
+            String a = reader.readLine();
+            String b = reader.readLine();
+            String c = reader.readLine();
+            int bs=Integer.parseInt(b);
+            int cs=Integer.parseInt(c);
+            System.out.println(a + " получает " + b + " через " + c + " лет.");//напишите тут ваш код
+
+        }
+    }
+
