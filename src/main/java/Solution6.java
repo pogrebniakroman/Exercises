df477cc644936626b63c35203968d279507fbd9a
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2019-12-21 23:25:58 +0200

    Default Changelist

---
+++ src/main/java/Solution6.java
@@ -0,0 +1,18 @@
+
+public class Solution6 {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        cat1.count++;
+
+
+        Cat cat2 = new Cat();
+        cat2.count++;
+
+
+        System.out.println("The cat count is " + Cat.count);
+    }
+
+    public static class Cat {
+        public static int count = 0;
+    }
+}
