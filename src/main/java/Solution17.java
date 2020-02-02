bb2a7c69c219c0d5525c7705888d6e906923ba50
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-02-02 18:14:13 +0200

    Default Changelist

---
+++ src/main/java/Solution17.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution17 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(reader.readLine());
+        for(int i=0; i<m; i++){
+            for(int j=1; j<n; j++){
+
+                System.out.print(8);
+            }
+            System.out.println(8);
+        }
+    }
+}
