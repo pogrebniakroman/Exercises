f11861e56a2b589f1936e6ad831a9c5fab36e9b4
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-02-05 21:04:17 +0200

    Default Changelist

---
+++ src/main/java/Solution18.java
@@ -0,0 +1,17 @@
+
+
+    public class Solution18 {
+        public static void main(String[] args) {
+            for(int i=1 ; i<=10; i++){
+                for(int j=0; j<i; j++){
+                    System.out.print(8);
+                }
+                System.out.println();
+
+            }
+
+        }
+    }
+
+
+
