3c3b09461245084f72b9eb2dd75839140369504e
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-02-05 21:22:16 +0200

    Default Changelist

---
+++ src/main/java/Solution19.java
@@ -0,0 +1,13 @@
+
+    import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+    public class Solution19 {
+        public static void main(String[] args) throws Exception {
+            BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+            String a=reader.readLine();
+            for(int i = 1; i <= 10; i++){
+                System.out.println(a+ " любит меня.");
+            }
+        }
+    }
