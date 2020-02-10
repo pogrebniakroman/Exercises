4e3a54b92ea0b1c9ca458f53e57326c3febe793b
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-02-10 20:42:17 +0200

    Default Changelist

---
+++ src/main/java/Dog22.java
@@ -0,0 +1,19 @@
+public class Dog22 {
+    String name;
+    int age;
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public int getAge(){
+        return age;
+    }
+    public static void main(String[] args) {
+
+    }
+}
