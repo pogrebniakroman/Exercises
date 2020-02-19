f23834e2504892759159f27bfb635d1f88236e18
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-02-19 21:38:04 +0200

    Default Changelist

---
+++ src/main/java/Solution24.java
@@ -0,0 +1,32 @@
+public class Solution24 {
+    private String name;
+    private int age;
+    private char sex;
+
+    public  void setName(String name){
+        this.name=name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public  void setAge(int age){
+        this.age=age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public  void setSex(char sex){
+        this.sex=sex;
+    }
+
+    public char getSex(){
+        return sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
