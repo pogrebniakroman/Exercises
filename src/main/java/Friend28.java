5dddd39d41f0c6648ea94ca6226a81676bf5df9f
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-02-19 23:15:08 +0200

    Default Changelist

---
+++ src/main/java/Friend28.java
@@ -0,0 +1,24 @@
+public class Friend28 {
+    String name;
+    int age;
+    char sex;
+    public Friend28(int age){
+        this.age = age;
+    }
+    public Friend28(String name){
+        this.name = name;
+    }
+    public Friend28(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public Friend28(String name, int age,char sex){
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Solution28.java
@@ -0,0 +1,24 @@
+public class Solution28 {
+    String name;
+    int age;
+    char sex;
+    public Friend(int age){
+        this.age=age;
+    }
+    public Friend(String name){
+        this.name=name;
+    }
+    public Friend(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    public Friend(String name, int age,char sex){
+        this.name=name;
+        this.age=age;
+        this.sex=sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
