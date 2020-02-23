7a519e3cf635a75bd558f2a1c7ae9d23a350bbce
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-02-23 17:51:36 +0200

    Default Changelist

---
+++ src/main/java/Circle37.java
@@ -0,0 +1,35 @@
+public class Circle37 {
+
+    public double x;
+    public double y;
+    public double radius;
+    public double z;
+
+    public Circle37(){
+    }
+
+    public Circle37(double x, double y, double radius){
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+    }
+
+    public Circle37( double y, double radius){
+        this.y=y;
+        this.radius=radius;
+    }
+
+    public Circle37(double x){
+        this.x=x;
+    }
+    public Circle37(double x, double y, double radius, double z){
+        this.x=x;
+        this.y=3.0;
+        this.radius=radius;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
