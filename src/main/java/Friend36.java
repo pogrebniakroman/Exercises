be6642e598b8b644ba7dad3e9bae4189b066c70f
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-02-23 17:15:56 +0200

    Default Changelist

---
+++ src/main/java/Friend36.java
@@ -0,0 +1,23 @@
+public class Friend36 {
+
+    String name;
+    int age;
+    char sex;
+
+    public void initialize(String name){
+        this.name = name;
+    }
+    public void initialize(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public void initialize(String name, int age, char sex){
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
