e9dc43557fde066dccade7492ac94aee04f61852
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-02-23 14:21:22 +0200

    Default Changelist

---
+++ src/main/java/Circle31.java
@@ -0,0 +1,27 @@
+public class Circle31 {
+
+    int centerX, centerY, radius, width,color;
+    public Circle31(int centerX, int centerY, int radius){
+        this.centerX=centerX;
+        this.centerY=centerY;
+        this.radius=radius;
+    }
+    public Circle31(int centerX, int centerY, int radius, int width){
+        this.centerX=centerX;
+        this.centerY=centerY;
+        this.radius=radius;
+        this.width=width;
+    }
+    public Circle31(int centerX, int centerY, int radius, int width, int color){
+        this.centerX=centerX;
+        this.centerY=centerY;
+        this.radius=radius;
+        this.width=width;
+        this.color=color;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
