5094998f795dd44a94c7bd707ce0eb2a5a2506fc
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-02-23 13:29:45 +0200

    Default Changelist

---
+++ src/main/java/Dog29.java
@@ -0,0 +1,22 @@
+public class Dog29 {
+
+    String name;
+    int height;
+    String color;
+
+
+    public Dog29  (String name){
+        this.name=name;
+    }
+    public Dog29(String name, int height){
+        this.name=name;
+        this.height= height;}
+
+    public Dog29(String name, int height, String color){
+        this.name=name;
+        this.height= height;
+        this.color= color;
+    }
+    public static void main(String[] args) {
+    }
+}
