a37239f0c48070006a94b6f2ad6ed16e853106e5
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-03-02 21:04:55 +0200

    Default Changelist

---
+++ src/main/java/Solutions/StringHelper.java
@@ -0,0 +1,23 @@
+package Solutions;
+
+public class StringHelper {
+
+    public static String multiply(String s) {
+        String result = "";
+        for(int i=0; i<5; i++){
+            result+=s;
+        }
+        return result;
+    }
+
+    public static String multiply(String s, int count) {
+        String result = "";
+        for(int i=0; i<count; i++){
+            result+=s;}
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
