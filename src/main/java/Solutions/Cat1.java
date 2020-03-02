397a6363ae03b4d3ee9cb57ef645071b6daf203a
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-03-02 19:29:53 +0200

    Default Changelist

---
+++ src/main/java/Solutions/Cat1.java
@@ -0,0 +1,23 @@
+package Solutions;
+
+public class Cat1 {
+
+    private static int catCount = 0;
+
+    public Cat1() {
+        catCount++;
+    }
+
+    public static int getCatCount() {
+        return
+                catCount;
+    }
+    public static void setCatCount(int catCount) {
+        Cat1.catCount = catCount;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
