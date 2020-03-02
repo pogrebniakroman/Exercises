6cda7d7c5c95c6ca1f34c1ee13c1876f60f6417e
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-03-02 20:17:57 +0200

    Default Changelist

---
+++ src/main/java/Solutions/ConsoleReader.java
@@ -0,0 +1,33 @@
+package Solutions;
+import java.io.*;
+public class ConsoleReader {
+
+    public static String readString() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String a = reader.readLine();
+        return a;
+
+    }
+
+    public static int readInt() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int b = Integer.parseInt(reader.readLine());
+        return b;
+    }
+
+    public static double readDouble() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double c = Double.parseDouble(reader.readLine());
+        return c;
+    }
+
+    public static boolean readBoolean() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean d = Boolean.parseBoolean(reader.readLine());
+        return d;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
