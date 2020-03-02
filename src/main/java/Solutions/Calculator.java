9934e9ec1c383d4f4b37805022c10a0cab35b653
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-03-02 21:34:28 +0200

    Default Changelist

---
+++ src/main/java/Solutions/Calculator.java
@@ -0,0 +1,33 @@
+package Solutions;
+
+public class Calculator {
+
+    public static int plus(int a, int b) {
+
+        return a+b;
+    }
+
+    public static int minus(int a, int b) {
+
+        return a-b;
+    }
+
+    public static int multiply(int a, int b) {
+
+        return a*b;
+    }
+
+    public static double division(int a, int b) {
+
+        return (double)a/b;
+    }
+
+    public static double percent(int a, int b) {
+
+        return (double)a/100*b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
