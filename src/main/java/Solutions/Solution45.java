c1c452f45d636f277d84622298fa9a931202f1bb
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-03-05 20:57:21 +0200

    Default Changelist

---
+++ src/main/java/Solutions/Solution45.java
@@ -0,0 +1,31 @@
+package Solutions;
+
+public class Solution45 {
+
+    public static int step;
+
+    public static void main(String[] args) {
+        method1();
+    }
+
+    public static void method1() {
+        method2();
+    }
+
+    public static void method2() {
+        new Solution45().method3();
+    }
+
+    public void method3() {
+        method4();
+    }
+
+    public  void method4() {
+        step++;
+        for (StackTraceElement element : Thread.currentThread().getStackTrace())
+            System.out.println(element);
+        if (step > 1)
+            return;
+        main(null);
+    }
+}
