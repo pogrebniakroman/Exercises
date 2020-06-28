83299c01848536afb4d6d703d30a02a17d41bfe2
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-06-28 19:44:36 +0300

    Default Changelist

---
+++ src/main/java/Solutions/Solution66.java
@@ -0,0 +1,32 @@
+package Solutions;
+
+
+public class Solution66 {
+    public static void main(String[] args) {
+    }
+
+    public class Animal {
+
+    }
+
+    public class Cow extends Animal {
+
+    }
+
+    public class Pig extends Animal {
+
+    }
+
+    public class Sheep extends Animal{
+
+    }
+
+    public class Bull extends Animal{
+
+    }
+
+    public class Goat extends Animal{
+
+    }
+
+}
