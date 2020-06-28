42fd443cf7ad6777a0c01b924031cd6171ee10c6
pogrebniakroman <43217544+pogrebniakroman@users.noreply.github.com>
2020-06-28 19:47:19 +0300

    Default Changelist

---
+++ src/main/java/Solutions/Solution67.java
@@ -0,0 +1,23 @@
+package Solutions;
+
+
+public class Solution67 {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class Chief extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee{
+
+    }
+}
